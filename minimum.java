77ddf4945a6ed83f75b97c08563dbc189995b9e2
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-07-31 12:20:21 +0530

    Create minimum.java

---
+++ minimum.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+import java.util.Scanner.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,min;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		min=a[0];
+		for(int i=1;i<n;i++)
+		{
+			if(min>a[i])
+			{
+				min=a[i];
+			}
+		}
+		System.out.println(min);
+	}
+}
